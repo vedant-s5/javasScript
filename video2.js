a7900afbb0c9f41f663dcb8a4dd33f76b0393aee
vedant-s5 <vedant.singh0507@gmail.com>
2025-09-02 20:45:44 +0530

    video2: write first code of js

---
+++ video2.js
@@ -0,0 +1,7 @@
+console.log("vedant")
+/* in todays time java script is not only used for frontend, it is now can 
+be used for backend and used in mobiles*/
+
+/*so, in this video I have learnt about how can i execute my program which 
+i have written above by writing (node 'file name') on the terminal and 
+which version of node js i am using by wrting (node -v).*/ 
