2f6f90a16b1a2531886a495c48fd51f4eea13e74
vedant-s5 <vedant.singh0507@gmail.com>
2025-09-03 12:38:16 +0530

    video5: learn datatypes and ecma standards

---
+++ video5.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+/* syntax ke bad semicolon lagaye ya na lagaye koi dikkat nhi hai just,
+na use kre to better hai*/
+
+//alert(3
+// +3)//aise nhi likhna chaiye, code readability should be high
+
+let name = "vedant"
+let age = 18
+let isloggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => this is a standalone value, null is an object
+//undefined => this comes when a variable is defined but the value in it is not defined
+//symbol => unique
+
+//object
+console.log(typeof age)
