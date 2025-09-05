a599e4400a9ca32e98a3e55bac5def5e57d71eb8
vedant-s5 <vedant.singh0507@gmail.com>
2025-09-05 12:26:02 +0530

    video7: learn why string to number conversion is confusion

---
+++ video7.js
@@ -0,0 +1,46 @@
+// OPERATIONS //
+
+let value = 3
+let negvalue = -value  
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(4/3);
+// console.log(2%3);
+
+let str1 = "vedant "
+let str2 = "singh"
+let str3 = str1 + str2
+console.log(str3)// vedant singh
+
+//console.log("1"+2)// 12
+//console.log(1+"2")// 12
+//console.log("1" + 2 + 2)//the output will be 122, starting main string hai toh conversion string main hoga
+//console.log(1 + 2 +"2")//the output will be 32, ismain string se pehle number hai toh conversion first number main hua phir string add hua
+
+//console.log(+true)//this will give 1 but if + is after true, it will give error
+
+//console.log(+"")// will give 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+//console.log(num1,num2,num3)
+
+let gamecounter = 100
+gamecounter++
+
+console.log(gamecounter)
+/*prefix(++gamecounter) and postffix(gamecounter++) ke bare main padhne 
+ke liye 'mdn' ya fir 'js' pe jake padhna hai
+output dono ka same hi aata hia but difference hota hai dono main*/
+
+// ecma type conversion read
+
+
+
+
+
+
